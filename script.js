02b1ad24a9d286687dc51dad30a843ce032ff288
NausheenSalauddin <w0805026@myscc.ca>
2022-09-15 16:36:17 -0400

    updated script.js to calculate amount

--- script.js
+++ script.js
@@ -9,10 +9,22 @@
 
 //function that checks if document is ready
 $(document).ready(function(){
-	
+
 	//add event handler
 	$('input').on('change',function(){
-        
+
+		//get the value of salary from HTML input 
+		let $salary = $('#yearly-salary').val();
+
+		//get the value of percentage from HTML input 
+		let $percent = $('#percent').val();
+
+		//calculate the amount that can be spent on tech
+        let $amountToSpend = $salary * $percent / 100;
+
+		//append the amount to HTML <p> tag
+		$('#amount').text("$" + $amountToSpend);
+		console.log($amountToSpend);
     });
 });
 
