8d914e97418cf825c1fdd9022d0264777e259458
NausheenSalauddin <w0805026@myscc.ca>
2022-09-15 15:53:54 -0400

    add author and file details to all files

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 /*
+    Filename: script.js
+    Author: Nausheen Salauddin
+    Student Number: 0805026
+    Date: 15-09-2022
 	WEB 303 Assignment 1 - jQuery
-	{Your Name Here}
+	{Nausheen Salauddin}
 */
+
+//function that checks if document is ready
+$(document).ready(function(){
+	//add event handler
+	$('input').on('change',function(){
+        
+    });
+});
 
