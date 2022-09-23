5f999f69cb8f68638fd8dc0844e0c79442440efd
NausheenSalauddin <w0805026@myscc.ca>
2022-09-23 12:11:32 -0400

    added click event

--- js/main.js
+++ js/main.js
@@ -6,4 +6,57 @@
 */
 // WEB303 Assignment 2
 
+$(document).ready(function() {
+    $('#prospect').on('click' , function () {
+
+        var xhr = new XMLHttpRequest;
+        xhr.open('GET','prospect.html',true);
+        xhr.onload = function()
+        {
+            if(this.status=== 200)
+            {
+                console.log("hello");
+                let textContent = $("#info");
+                textContent.html(xhr.responseText);
+                //textContent.slideDown("slow");
+            }
+        }   
+        xhr.send();
+    }) 
+
+    $('#convert').on('click' , function () {
+
+        var xhr = new XMLHttpRequest;
+        xhr.open('GET','convert.html',true);
+        xhr.onload = function()
+        {
+            if(this.status=== 200)
+            {
+
+                let textContent = $("#info");
+                textContent.html(xhr.responseText);
+                //textContent.slideDown("slow");
+            }
+        }   
+        xhr.send();
+    }) 
+
+    $('#retain').on('click' , function () {
+
+        var xhr = new XMLHttpRequest;
+        xhr.open('GET','retain.html',true);
+        xhr.onload = function()
+        {
+            if(this.status=== 200)
+            {
+                let textContent = $("#info");
+                textContent.html(xhr.responseText);
+                //textContent.slideDown("slow");
+            }
+        }   
+        xhr.send();
+    }) 
+
+})
+
 
