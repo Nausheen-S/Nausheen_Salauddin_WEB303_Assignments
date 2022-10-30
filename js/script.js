42e260827c112383f1356420547d69fe13e06e9d
NausheenSalauddin <w0805026@myscc.ca>
2022-10-30 18:22:26 -0400

    trying accordian logic

--- js/script.js
+++ js/script.js
@@ -3,4 +3,18 @@
     Author: Nausheen Salauddin
     Student Number: 0805026
     Date: 29-10-2022
-    */
+*/
+
+$(document).ready(function () {
+    $(".accordian-control").on("click", ".accordion-control", function (e) {
+        //e.preventDefault();
+        if (false == $(this).next().is(':visible')) {
+        
+            $('.accordion-panel').slideUp(300);
+        
+        }
+        $(this).next(".accordion-panel").slideToggle();
+        console.log("hello");
+    });
+
+});
