1b86fc9dd8795ab4a5d617468aa08212beb96591
NausheenSalauddin <w0805026@myscc.ca>
2022-10-31 12:49:46 -0400

    added accordian functionality

--- js/script.js
+++ js/script.js
@@ -8,13 +8,38 @@
 $(document).ready(function () {
 
     /***************  ACCORDIAN *****************/
-    let allPanels = $('.accordion-panel').hide();
-    $('.accordion-one > li > button').on('click', function (e) {
-        // allPanels.slideUp();
-        console.log($(this))
-        $(this).parent().next().slideDown();
-        return false;
-    });
+
+    /** Accordian 1 **/
+    $('section:nth-of-type(1) ul li button').on('click', function (e) {
+        e.preventDefault();
+        let $this = $(this);
+
+        $('section:nth-of-type(1) ul li button').each(function(){
+            $this.removeClass("showing");
+        });
+
+        $this.toggleClass('showing');
+        $this.next().slideToggle();
+
+        //Hide the other panels
+        $("section:nth-of-type(1) ul li div").not($this.next()).slideUp();
+
+    })
+
+     /** Accordian 2 **/
+    $('section:nth-of-type(2) ul li button').on('click', function (e) {
+        e.preventDefault();
+        let $this = $(this);
+        $('section:nth-of-type(2) ul li button').each(function(){
+            $this.removeClass("showing");
+        });
+
+        $this.toggleClass('showing');
+        $this.next().slideToggle();
+
+        //Hide the other panels
+        $("section:nth-of-type(2) ul li div").not($this.next()).slideUp();
+    })
 
     /***************  TABS *****************/
     $('.tab-panel').hide();
