20bf10c1863f99f3d0ada80bedc65866f541410c
NausheenSalauddin <w0805026@myscc.ca>
2022-11-25 12:06:23 -0500

    added html form

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/*
+    Filename: script.js
+    Author: Nausheen Salauddin
+    Student Number: 0805026
+    Date: 19-11-2022
+ */
+
+//function that checks if document is ready
+$(document).ready(function () {
+
+    /************ Name *************/
+    //add name 
+    $('h1').text("Nausheen Salauddin");
+})
