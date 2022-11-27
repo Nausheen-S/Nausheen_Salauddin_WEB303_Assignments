47f6ec62ed3b59908684f3f61c56fa7c187c8781
NausheenSalauddin <w0805026@myscc.ca>
2022-11-27 11:53:34 -0500

    added form validation

--- js/script.js
+++ js/script.js
@@ -8,18 +8,14 @@
 //function that checks if document is ready
 $(document).ready(function () {
 
-    /************ Name *************/
     //add name 
     $('h1').text("Nausheen Salauddin");
 
     //form submit button disabled using javascript
     let submitBtn = document.getElementById('submit');
+    submitBtn.classList.remove('enabled');
     submitBtn.disabled = true;
-    submitBtn.classList.add('disabled')
-    submitBtn.style.backgroundColor =  'lightGray';
-    submitBtn.style.cursor = 'default';
-
-    //add validation for form
+    submitBtn.classList.add('disabled');
     
     //add select box
     let selectElement = document.getElementById('country');
@@ -33,13 +29,83 @@ $(document).ready(function () {
     //submit form message
     let msgElement = document.createElement('h2');
     let formElement = document.getElementById('validateForm');
+    let elements = formElement.elements;    
     submitBtn.addEventListener('click', function(e) {            
-        e.preventDefault();                             
-        let elements = formElement.elements;                   
+        e.preventDefault();                                            
         let username = elements.username.value;     
         let optionChosen = elements.country.value;
         let msg = `Welcome ${username}! The country code you selected is ${optionChosen}`;  
         msgElement.textContent = msg;       
         formElement.after(msgElement); 
     })
+
+    //add validation for form
+    let userField = document.getElementById('username');
+    let pswrdField = document.getElementById('password');
+    let confirmPswrdField = document.getElementById('confirmPassword');
+    let checkField = document.getElementById('terms');
+    let optionChosen = elements.country.value;
+    let allInputs = document.querySelectorAll('#validateForm input');
+
+    let validateInput1 = function () {
+        return (userField.value.length > 0);
+    }
+
+    let validateInput2 = function () {
+        return (pswrdField.value.length >= 12);
+    }
+
+    let validateInput3 = function () {
+        return (confirmPswrdField.value === pswrdField.value);
+    }
+
+    let validateInput4 = function () {
+        return (checkField.checked === true);
+    }
+
+    let validateInput5 = function () {
+        return (optionChosen.value != "defaultOption");
+    }
+
+    let validateFormFunc = function () {
+
+        if (!validateInput1()) {
+            return false;
+        }
+
+        if (!validateInput2()) {
+            return false;
+        }
+
+        if (!validateInput3()) {
+            return false;
+        }
+
+        if (!validateInput4()) {
+            return false;
+        }
+
+        if (!validateInput5()) {
+            return false;
+        }
+        return true;
+    }
+
+    for (let i = 0, length = allInputs.length; i < length; i++) {
+        let checkValid = function () {      
+            if ( !validateFormFunc() )
+            {
+                submitBtn.disabled = true;
+                submitBtn.classList.add('disabled')
+            }
+            else
+            {
+                submitBtn.disabled = false;
+                submitBtn.classList.remove('disabled')
+            }
+        }
+        allInputs[i].addEventListener('change', checkValid);
+        allInputs[i].addEventListener('keyup', checkValid);
+    }
 })
+
